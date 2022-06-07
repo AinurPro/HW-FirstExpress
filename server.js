0099c2267ace9296a00d6c9bbc716642ce2191ee
AinurPro <aakhmetcheyeva@gmail.com>
2022-06-06 23:01:24 -0400

    updated

--- server.js
+++ server.js
@@ -30,11 +30,10 @@ app.get('/tip/:total/:tipPercentage', (req, res)=> {
     res.send(req.params.tipPercentage)
     })
 
-// Your app should have a route of '/tip' and it should expect 2 params. One should be total and one should be tipPercentage.
-
-// When hitting the route, the page should display
-// how much your tip will be based on the total amount of the bill and the tip percentage. (ex. hitting '/tip/100/20' should display 20 on the page).
-
+// Magic 8 ball
+// app.get('/magic', (req, res)=> {
+//     res.send(`<h1>["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]</h1>`)
+// })
 
 
 app.listen(PORT, ()=> {
