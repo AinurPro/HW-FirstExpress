71ff69477555a4df43b88fd99d7349991e847d84
AinurPro <aakhmetcheyeva@gmail.com>
2022-06-08 17:59:27 -0400

    changes in server.js

--- server.js
+++ server.js
@@ -34,6 +34,15 @@ app.get('/tip/:total/:tipPercentage', (req, res)=> {
 // app.get('/magic', (req, res)=> {
 //     res.send(`<h1>["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]</h1>`)
 // })
+let magicEight = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely",
+"You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", 
+"Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", 
+"Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+
+app.get('/magic/:question', (req,res)=>{
+    res.send(`<h1> Question: ${req.params.question} </h1>
+    <h2> Answer : ${magicEight[Math.floor(Math.random()*magicEight.length)]}</h2>`)
+})
 
 
 app.listen(PORT, ()=> {
